a4f2ea5ea281e129aff727402f674f558e32e2e2
Santiago Trini <santiagotrini@gmail.com>
2023-08-14 14:48:03 -0300

    agrega cosas

--- src/List.js
+++ src/List.js
@@ -1,18 +1,20 @@
 import ListItem from "./ListItem";
 import data from './data';
+
+
 const List = () => {
 
 
 
   return (
     <ul>
-      {data.map(item => (
+      {data.map(item => 
         <ListItem 
           key={item.id} 
           text={item.text} 
           done={item.done}
         />
-      ))}  
+      )}  
     </ul>
   );
 };
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,17 +1,45 @@
+import { useState } from 'react';
+
 const ListItem = props => {
+  
+
+  const [done, setDone] = useState(props.done); // hook de estado
+  // const done = array[0];
+  // const setDone = array[1];
+ 
+
   let style = {};
-  if (props.done) {
+  if (done) {
     style.textDecoration = 'line-through';
   }
 
+
+
 //   let style = props.done ? { textDecoration: 'line-through' } : {}; 
+
+  const handleChange = event => {
+    // console.log(event.target.checked);
+    setDone(done => !done);
+  };
+
 
   return (
     <li>
-        <input checked={props.done} type="checkbox" />
+        <input  
+          checked={done}
+          onChange={handleChange} 
+          type="checkbox" 
+        />
         <label style={style}>{props.text}</label>
+        {done ? <button>X</button> : null}
+        {done && <button>X</button>}
     </li>
   );  
 };
 
 export default ListItem;
+
+
+
+
+
